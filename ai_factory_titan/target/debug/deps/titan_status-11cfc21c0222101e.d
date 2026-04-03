3165a4a46af9d94dc16c89dbde940e020dc468a8
super244 <ceosomeone105@gmail.com>
2026-04-03 09:46:01 -0700

    chore: clean up target build artifacts

--- ai_factory_titan/target/debug/deps/titan_status-11cfc21c0222101e.d
+++
@@ -1,5 +0,0 @@
-/Users/luca/Projects/ai-factory/ai_factory_titan/target/debug/deps/titan_status-11cfc21c0222101e.d: src/bin/titan-status.rs
-
-/Users/luca/Projects/ai-factory/ai_factory_titan/target/debug/deps/titan_status-11cfc21c0222101e: src/bin/titan-status.rs
-
-src/bin/titan-status.rs:
